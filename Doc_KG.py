ed5f4bada340612f6a56972fecb69651d078495e
Hardik <hardikgw@gmail.com>
2019-09-14 17:21:23 -0400

    cp

--- Doc_KG.py
+++ Doc_KG.py
@@ -1,3 +1,14 @@
+import pandas as pd
+import numpy as np
+import spacy
+from tqdm import tqdm
+import re
+import time
+import pickle
+import tensorflow_hub as hub
+import tensorflow as tf
+pd.set_option('display.max_colwidth', 200)
+
 class DocEmbedding:
     def __init__(self):
         self.lines_to_process = 10
