375c8b612fc2fd5048a7d511cc0d8e3da33a660e
Hardik <hardikgw@gmail.com>
2019-09-14 17:00:26 -0400

    cp

--- Doc_KG.py
+++ Doc_KG.py
@@ -1,17 +1,17 @@
-
-
 class DocEmbedding:
     def __init__(self):
-        self.lines_to_process = -1
+        self.lines_to_process = 10
 
     def generate_embeddings(self, file_path):
         f = open(file_path)
         for index, line in enumerate(f):
-            print(line)
+            fields = line.split('\t')
             if index == 0:
                 continue
+            elif index > self.lines_to_process or self.lines_to_process < 0:
+                break
         f.close()
 
 
 embeddings = DocEmbedding()
-embeddings.generate_embeddings('/Users/hardikpatel/workbench/data/patent/text.txt')
+embeddings.generate_embeddings('/Users/hardikpatel/workbench/data/patent/brf_sum_text.tsv')
