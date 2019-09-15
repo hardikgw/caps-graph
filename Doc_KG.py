051de03243f0c1e561b5ed3e77010beed62c038d
Hardik <hardikgw@gmail.com>
2019-09-15 09:10:57 -0400

    cp

--- Doc_KG.py
+++ Doc_KG.py
@@ -7,7 +7,14 @@ import time
 import pickle
 import tensorflow_hub as hub
 import tensorflow as tf
+import warnings
+
+warnings.filterwarnings('ignore')
 pd.set_option('display.max_colwidth', 200)
+from sklearn.metrics.pairwise import cosine_similarity
+
+elmo = hub.Module("https://tfhub.dev/google/elmo/2", trainable=True)
+
 
 class DocEmbedding:
     def __init__(self):
@@ -17,12 +24,29 @@ class DocEmbedding:
         f = open(file_path)
         for index, line in enumerate(f):
             fields = line.split('\t')
+            print(fields)
             if index == 0:
                 continue
             elif index > self.lines_to_process or self.lines_to_process < 0:
                 break
         f.close()
 
+    def elmo_vectors(x):
+        embeddings = elmo(x, signature="default", as_dict=True)["elmo"]
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            sess.run(tf.tables_initializer())
+            # return average of ELMo features
+            return sess.run(tf.reduce_mean(embeddings, 1))
 
-embeddings = DocEmbedding()
-embeddings.generate_embeddings('/Users/hardikpatel/workbench/data/patent/brf_sum_text.tsv')
+        elmo_embeddings = []
+        print(len(corpus))
+        for i in range(len(corpus)):
+            print(corpus[i])
+            elmo_embeddings.append(elmo_vectors([corpus[i]])[0])
+        print(elmo_embeddings)
+        print(cosine_similarity(elmo_embeddings, elmo_embeddings))
+
+
+doc_embeddings = DocEmbedding()
+doc_embeddings.generate_embeddings('/Users/hardikpatel/workbench/data/patent/brf_sum_text.tsv')
