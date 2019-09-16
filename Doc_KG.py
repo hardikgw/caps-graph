e3e77011fb78f5ae6598c02c8405fd979e0749cb
Hardik <hardikgw@gmail.com>
2019-09-15 23:18:58 -0400

    cp

--- Doc_KG.py
+++ Doc_KG.py
@@ -1,19 +1,43 @@
 import pandas as pd
 import numpy as np
 import spacy
+from allennlp.commands.elmo import ElmoEmbedder
 from tqdm import tqdm
 import re
 import time
 import pickle
+import h5py
 import tensorflow_hub as hub
 import tensorflow as tf
 import warnings
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 warnings.filterwarnings('ignore')
 pd.set_option('display.max_colwidth', 200)
 from sklearn.metrics.pairwise import cosine_similarity
 
 elmo = hub.Module("https://tfhub.dev/google/elmo/2", trainable=True)
+elmoPT = ElmoEmbedder(
+    options_file='https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json',
+    weight_file='https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5'
+)
+
+from allennlp.modules.elmo import Elmo, batch_to_ids
+
+options_file = "https://allennlp.s3.amazonaws.com/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+weight_file = "https://allennlp.s3.amazonaws.com/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+
+# Compute two different representation for each token.
+# Each representation is a linear weighted combination for the
+# 3 layers in ELMo (i.e., charcnn, the outputs of the two BiLSTM))
+elmo = Elmo(options_file, weight_file, 2, dropout=0)
+
+# use batch_to_ids to convert sentences to character ids
+sentences = [['First', 'sentence', '.'], ['Another', '.']]
+character_ids = batch_to_ids(sentences)
+
 
 
 class DocEmbedding:
